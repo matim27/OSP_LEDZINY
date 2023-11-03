108c72ce249397adb7bada240a4f375ac6126f30
Mateusz Musioł <mati.m27@wp.pl>
2023-11-03 15:32:54 +0100

    tests

--- OSP_APP/OSP_APP/settings.py
+++ OSP_APP/OSP_APP/settings.py
@@ -189,3 +189,4 @@ app.config_from_object('django.conf:settings')
 TWILIO_ACCOUNT_SID = env('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = env('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = env('TWILIO_PHONE_NUMBER')
+
--- OSP_APP/custom_auth/tests.py
+++ OSP_APP/custom_auth/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
+from .models import CustomUserManager
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.mark.django_db
+def test_create_user():
+    manager = CustomUserManager()
+    user = manager.create_user(email="test@example.com", password="testpassword")
+    assert user.email == "test@example.com"
+    assert user.is_active is True
+    assert user.is_staff is False
+    assert user.is_superuser is False
+
+@pytest.mark.django_db
+def test_create_superuser():
+    manager = CustomUserManager()
+    user = manager.create_superuser(email="admin@example.com", password="adminpassword")
+    assert user.email == "admin@example.com"
+    assert user.is_active is True
+    assert user.is_staff is True
+    assert user.is_superuser is True
+
+@pytest.mark.django_db
+def test_create_initial_user():
+    manager = CustomUserManager()
+    user = manager.create_initial_user(email="new@example.com")
+    assert user.email == "new@example.com"
+    assert user.is_active is False
+    assert user.check_password("")
+
