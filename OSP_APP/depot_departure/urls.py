b5730b19bebf74828f3133d5b3899c4c8bcd7989
Mateusz Musioł <mati.m27@wp.pl>
2023-11-03 14:18:31 +0100

    Change css and add depot departure list view on site

--- OSP_APP/depot_departure/urls.py
+++ OSP_APP/depot_departure/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.static import static
 from django.urls import path
 from OSP_APP import settings
-from depot_departure.views import DepotDepartureAddView
+from depot_departure.views import DepotDepartureAddView, DepotDepartureListView
 
 urlpatterns = [
-    path('depot/add/', DepotDepartureAddView.as_view(), name='home'),
+    path('depot/add/', DepotDepartureAddView.as_view(), name='depot_add'),
+    path('depot/list/', DepotDepartureListView.as_view(), name='depot_list'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- OSP_APP/depot_departure/views.py
+++ OSP_APP/depot_departure/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from depot_departure.forms import DepotDepartureForm
 from depot_departure.models import DepotDeparture
@@ -11,3 +11,15 @@ class DepotDepartureAddView(CreateView):
     model = DepotDeparture
     form_class = DepotDepartureForm
     success_url = reverse_lazy('home')
+
+
+class DepotDepartureListView(ListView):
+    model = DepotDeparture
+    template_name = 'depot_departure/list/depot_departure_list.html'
+    context_object_name = 'depot'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        last_depot_departure = DepotDeparture.objects.order_by('-trip_id')
+        context['last_depot'] = last_depot_departure
+        return context
