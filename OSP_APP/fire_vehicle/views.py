f3087b8935b86d7ed52095a0af5d79936be0a8d6
Mateusz Musioł <mati.m27@wp.pl>
2023-11-03 15:06:23 +0100

    fixes

--- OSP_APP/fire_vehicle/views.py
+++ OSP_APP/fire_vehicle/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView
 
+from depot_departure.models import DepotDeparture
 from fire_vehicle.models import FireVehicle
 
 
@@ -8,6 +9,11 @@ class GCBAView(ListView):
     model = FireVehicle
     template_name = 'fire_vehicle/gcba.html'
     context_object_name = 'firevehicle'
+    queryset = FireVehicle.objects.get(code_name='649[S]43')
 
-    def get_queryset(self):
-        return FireVehicle.objects.get(code_name='649[S]43')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vehicle_id = FireVehicle.objects.get(code_name='649[S]43')
+        depot_count = DepotDeparture.objects.filter(vehicles=vehicle_id).count()
+        context['depot_count'] = depot_count
+        return context
